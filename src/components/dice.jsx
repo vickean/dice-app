0d019a0dc24ffcf0ed1474101aa797df6c1fda0a
Victor Kean <vickean@gmail.com>
2019-07-28 14:04:45 +0800

    started rudimentary dice component

---
+++ src/components/dice.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Dice extends Component {
+  state = {};
+  render() {
+    return <button className="btn btn-primary m-2">Hello!</button>;
+  }
+}
+
+export default Dice;
